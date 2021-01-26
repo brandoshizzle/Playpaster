90ba4db03e71045c6165a9dfefd3eb5194d28eaa
brandoshizzle <cathcart.brandon@gmail.com>
2021-01-25 17:32:22 -0700

    Updates

---
+++ precache-manifest.3fa2bb5372332dd97ebab080193c3532.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f05a9e077d91e9bf41a",
+    "url": "/Playpaster/static/css/main.6df7395b.chunk.css"
+  },
+  {
+    "revision": "9f05a9e077d91e9bf41a",
+    "url": "/Playpaster/static/js/main.2d37df00.chunk.js"
+  },
+  {
+    "revision": "2a444a78cbd56588224d",
+    "url": "/Playpaster/static/js/runtime~main.8cb554cf.js"
+  },
+  {
+    "revision": "540b0ea875faeed41b29",
+    "url": "/Playpaster/static/js/2.212504e8.chunk.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/Playpaster/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "c54db7773284511750a692eae1b95107",
+    "url": "/Playpaster/static/media/_DSC0223.c54db777.jpg"
+  },
+  {
+    "revision": "4c20f02f47bb5b6c9ad5bae45f08bfa1",
+    "url": "/Playpaster/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/playpaster/precache-manifest.2c56e9a89ca49f6b9f9245aefe34ec53.js"
+  "/Playpaster/precache-manifest.3fa2bb5372332dd97ebab080193c3532.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/playpaster/index.html", {
+workbox.routing.registerNavigationRoute("/Playpaster/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
